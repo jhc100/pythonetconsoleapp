7cd7095fe3b283884bba0fb2cc577b8848ccde71
jhidalgo <j.hidalgo@outlook.com>
2023-05-15 17:09:25 -0600

    Add dotnet core code

---
+++ core/Program.cs
@@ -0,0 +1,16 @@
+﻿using Python.Runtime;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string pythonDll = @"C:\Python311\python311.dll";
+        Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", pythonDll);
+        PythonEngine.Initialize();
+        using (Py.GIL())
+        {
+            dynamic np = Py.Import("numpy");
+            Console.WriteLine(np.cos(np.pi * 2.5));
+        }
+    }
+}
