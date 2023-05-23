2cb99cf7408e5931cf4aab8ee702832f6ae11d48
unknown <xiuh.estebarra@gmail.com>
2023-05-23 12:57:19 -0600

    Adding python test file

--- core/Program.cs
+++ core/Program.cs
@@ -4,13 +4,16 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        string pythonDll = @"C:\Python311\python311.dll";
+        string pythonDll = @"C:\Users\Luis Estebarra\AppData\Local\Programs\Python\Python39\python39.dll";
         Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", pythonDll);
         PythonEngine.Initialize();
         using (Py.GIL())
         {
-            dynamic np = Py.Import("numpy");
-            Console.WriteLine(np.cos(np.pi * 2.5));
+            dynamic os = Py.Import("os");
+            dynamic sys = Py.Import("sys");
+            sys.path.append(os.path.dirname(os.path.expanduser("calculator.py")));
+            dynamic calculator = Py.Import(Path.GetFileNameWithoutExtension("calculator.py"));
+            Console.WriteLine(calculator.sum(2,3));
         }
     }
 }
